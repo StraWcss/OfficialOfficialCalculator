39a01e7cdd3fba20479fc7cbb7a262523b1e5515
John Markley <john.markley@dignityhealthcd.onmicrosoft.com>
2022-12-14 17:32:49 -0700

    added server and socket logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
 import { NumberButtonsComponent } from './number-buttons/number-buttons.component';
+import { SocketService } from './services/socket.service';
 import { SignalrService } from './signalr.service';
 
+const config: SocketIoConfig = {
+	url: 'http://localhost:3000', // socket server url;
+	options: {
+		transports: ['websocket']
+	}
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +24,10 @@ import { SignalrService } from './signalr.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    SocketIoModule.forRoot(config)
   ],
-  providers: [SignalrService],
+  providers: [SignalrService, SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
